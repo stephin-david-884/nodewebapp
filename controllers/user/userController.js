703493effe14ceecce1576dac463c53bb1d546ae
Stephin David <stephindavid884@gmail.com>
2025-05-04 13:37:21 +0530

    view engine set up

---
+++ controllers/user/userController.js
@@ -0,0 +1,23 @@
+
+const pageNotFound = async (req,res) => {
+    try {
+        res.render("page-404")
+    } catch (error) {
+        res.redirect("/pageNotFound")
+    }
+}
+
+const loadHomepage = async (req,res) => {
+    try {
+        
+        return res.render('home');
+
+    } catch (error) {
+        console.log("Home page not found");
+        res.status(500).send("Server error")
+    }
+    
+}
+
+
+module.exports = {loadHomepage, pageNotFound}
